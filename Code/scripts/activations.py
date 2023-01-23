729a1ba18a006f1506304fef3dcfe3c7dfa712ab
Bala-A87 <bala8702@gmail.com>
2023-01-23 10:49:43 +0530

    Try different custom activations

---
+++ Code/scripts/activations.py
@@ -0,0 +1,61 @@
+import torch
+
+def expNegMod(x):
+    """
+    For input x, returns exp(-abs(x))
+    """
+    return torch.exp(-torch.abs(x))
+
+class ExpNegMod(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def forward(self, x):
+        return expNegMod(x)
+
+def expNegSq(x):
+    """
+    For input x, returns exp(-x**2)
+    """
+    return torch.exp(-x**2)
+
+class ExpNegSq(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def forward(self, x):
+        return expNegSq(x)
+
+def discExpNegMod(x):
+    """
+    For input x, returns relu(1 - 0.5*relu(abs(x)))
+    """
+    relu = torch.nn.ReLU()
+    return relu(1 - 0.5*relu(torch.abs(x)))
+
+class DiscExpNegMod(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def forward(self, x):
+        return discExpNegMod(x)
+
+def mod(x):
+    return torch.abs(x)
+
+class Mod(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def forward(self, x):
+        return mod(x)
+
+def sq(x):
+    return x**2
+
+class Sq(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def forward(self, x):
+        return sq(x)
