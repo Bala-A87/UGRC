9bce5b366d4a90b23f842e879edb020f8ab8afcd
Bala-A87 <bala8702@gmail.com>
2023-01-27 14:42:58 +0530

    Add neg mse metric

--- Code/scripts/metrics.py
+++ Code/scripts/metrics.py
@@ -14,4 +14,19 @@ class BinaryAccuracy(torch.nn.Module):
     
     def forward(self, preds, y):
         preds_class = torch.round(preds)
-        return torch.tensor(torch.sum(preds_class == y)/len(y))
+        return torch.sum(preds_class == y)/len(y)
+
+class NegMeanSquaredError(torch.nn.Module):
+    """
+    Computes negative mean squared error for a regression problem.
+
+    Created object must be called with two arguments:
+        preds: Predicted values
+        y: True values
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def forward(self, preds, y):
+        return -torch.sum((preds.detach() - y)**2)/len(y)
