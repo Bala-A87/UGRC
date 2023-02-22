cb92b7d3ed8a46fcced72863ad5c150478c8ae71
Bala-A87 <bala8702@gmail.com>
2023-02-22 11:29:22 +0530

    Add versatility to model class

--- Code/scripts/models.py
+++ Code/scripts/models.py
@@ -2,16 +2,28 @@ from torch import nn
 
 class SimpleNN(nn.Module):
     """
-    Subclasses torch.nn.Module to implement a simple binary classification neural network.
+    Subclasses torch.nn.Module to implement a simple multi-purpose neural network.
 
     Args:
         input_size (int): Number of input features.
+        output_size (int, optional): Number of outputs to produce. Defaults to 1.
+        task_type (str, optional): Type of task to perform.
+            Allowed values:
+                'regression',
+                'classification'
+            Defaults to 'classification'.
+        The following combinations are possible:
+            output_size: 1, task_type: 'regression' => Classic regression
+            output_size: 1, task_type: 'classification' => Binary classification
+            output_size: >2, task_type: 'classification' => Multiclass classification
         hidden_layers (int, optional): Number of hidden layers in the model. Defaults to 2.
         hidden_units (int, optional): Number of neurons per hidden layer in the model. Defaults to 32.
     """
     def __init__(
         self,
         input_size: int,
+        output_size: int = 1,
+        task_type: str = 'classification',
         hidden_layers: int = 2,
         hidden_units: int = 32
     ) -> None:
@@ -26,10 +38,22 @@ class SimpleNN(nn.Module):
         for i in range(hidden_layers):
             self.hidden.append(nn.Linear(in_features=hidden_units, out_features=hidden_units))
             self.hidden.append(nn.ReLU())
-        self.output = nn.Sequential(
-            nn.Linear(in_features=hidden_units, out_features=1),
-            nn.Sigmoid()
-        )
+            
+        if task_type.lower() == 'regression':
+            self.output = nn.Sequential(
+                nn.Linear(in_features=hidden_units, out_features=output_size)
+            )
+        elif task_type.lower() == 'classification':
+            if output_size == 1:
+                self.output = nn.Sequential(
+                    nn.Linear(in_features=hidden_units, out_features=output_size),
+                    nn.Sigmoid()
+                )
+            else:
+                self.output = nn.Sequential(
+                    nn.Linear(in_features=hidden_units, out_features=output_size),
+                    nn.Softmax(dim=1)
+                )
     
     def forward(self, x):
         return self.output(self.hidden(self.input(x)))
